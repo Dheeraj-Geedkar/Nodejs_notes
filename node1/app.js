2d52f4ccf26c03df1e829c02bbe535e63cdd41ee
Dheeraj Geedkar <114930324+Dheeraj-Geedkar@users.noreply.github.com>
2025-02-17 11:59:30 +0000

    Configuration config done

---
+++ node1/app.js
@@ -0,0 +1,15 @@
+var name = 'hello world' ;
+var a = 10 ;
+var b = 20 ;
+
+// console.log(name);
+
+// console.log(a + b);
+
+// console.log(global);  Global object of node => global
+
+// console.log(this);   =>   {}   ( Empty object )
+
+// console.log(globalThis); => Points to or contains global object
+
+// console.log(globalThis === global);     => true
